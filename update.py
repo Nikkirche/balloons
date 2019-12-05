1f8569f414fb62c9aae1fec55d058ebe73ad4be7
Sergey Melnikov <svembox@gmail.com>
2019-12-05 15:43:10 +0000

    Do not filter teams in update.py, needed for correct remote-FTS processing

--- update.py
+++ update.py
@@ -61,8 +61,8 @@ def parse_pcms(data, *, callback_ok, callback_team, callback_problem, callback_c
                 callback_problem(problem.attrib['alias'], problem.attrib['name'])
         elif child.tag == 'session':
             id = child.attrib['alias']
-            if config.hall_by_team_name(id) is None:
-                continue
+#            if config.hall_by_team_name(id) is None:
+#                continue
             callback_team(id, child.attrib['party'])
             # callback_team(child.attrib['id'], child.attrib['party'])
             for problem in child:
